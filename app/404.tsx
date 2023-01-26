011987e42f2e55e75124781cc031a533b58055a9
ahamedzoha <ahamed.zoha@gmail.com>
2023-01-26 18:50:18 +0600

    fix: lints and typechecks

---
+++ app/404.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>Sorry, we couldn't find what you were looking for.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
